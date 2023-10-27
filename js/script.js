1ec10b76453474414220176049c625c55458f5e7
TomaszMianecki <6973773-TomaszMianecki@users.noreply.replit.com>
2023-10-27 12:58:34 +0000

    zapis prac nad stroną

--- js/script.js
+++ js/script.js
@@ -5,3 +5,28 @@ VanillaTilt.init(document.querySelectorAll(".glassmorphic-card"), {
   "max-glare": .1,
 });
 
+let flaga_menu_mobilne= false;
+let flaga_menu_lang_mobilne= false;
+function menu_togle(){
+  let lista_menu=document.getElementById("lista_menu");
+
+  if(flaga_menu_mobilne==false){
+    lista_menu.style.display="flex"
+  }
+  if(flaga_menu_mobilne==true){
+    lista_menu.removeAttribute("style");
+  }
+  flaga_menu_mobilne=!flaga_menu_mobilne;
+}
+
+function togle_lang(){
+  let lista_menu_2=document.getElementById("lista_menu_2");
+  
+  if(flaga_menu_lang_mobilne==false){
+      lista_menu_2.style.display="flex"
+  }
+  if(flaga_menu_lang_mobilne==true){
+      lista_menu_2.removeAttribute("style");
+  }
+    flaga_menu_lang_mobilne=!flaga_menu_lang_mobilne;
+}
